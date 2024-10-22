fe65d113f35b25ca57295a238a7475462dca4f97
TianziLuo <tluo023@ucr.edu>
2024-10-21 19:01:27 -0700

    student manager

---
+++ TS project/average_grade/src/student.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+//Interface names start with an uppercase letter
+interface Student{
+    id: number;
+    name: string;
+    age: number;
+    grades: number[];
+}
+
+class StudentManager{
+    
+}
