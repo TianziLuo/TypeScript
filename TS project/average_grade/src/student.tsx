579cdb89be1887db38d69dabd3c8dcf319fa35cd
TianziLuo <tluo023@ucr.edu>
2024-10-22 19:01:18 -0700

    class component

--- TS project/average_grade/src/student.tsx
+++ TS project/average_grade/src/student.tsx
@@ -1,13 +1,52 @@
-import React from "react"
+
+import React from 'react';
 
 //Interface names start with an uppercase letter
 interface Student{
-    id: number;
-    name: string;
-    age: number;
-    grades: number[];
+  id?: number;
+  name: string;
+  age?: number;
+  grades: number[];
+
+  calGrade(): number;
 }
 
-class StudentManager{
-  private student: Student[] = []  
+class StudentManager implements Student{
+  id?: number;
+  name: string;
+  age?: number;
+  grades: number[];
+
+  constructor (name: string, grades: number[], age?: number, id?: number){
+    this.name = name;
+    this.grades = grades;
+    if (id !== undefined) this.id = id;
+    if (age !== undefined) this.age = age;
+  }
+
+    calGrade(): number{
+    let sum: number = 0;
+    this.grades.forEach((number) =>{
+      sum += number;
+    })
+
+    const re: number = sum / this.grades.length;
+
+    return re    
+  }
 }
+
+
+
+const StudentComponent: React.FC = () =>{
+  const newStudent = new StudentManager('cg', [12,38,97,65]);
+
+  return(
+    <div>
+      <p>Student Name: {newStudent.name}</p>
+      <p>Average Grade: {newStudent.calGrade()}</p>
+    </div>
+  )
+};
+
+export default StudentComponent
